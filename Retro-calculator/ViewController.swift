22b0335c4c746f0eae937a2e14c0aca2ae1db775
gao830 <yg6c2@mst.edu>
2017-01-05 14:09:47 -0600

    Add sound

--- Retro-calculator/ViewController.swift
+++ Retro-calculator/ViewController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        do{
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+    }
+    @IBAction func btnPressed(sender: UIButton){
+        playSound()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func playSound(){
+        if btnSound.isPlaying{
+            btnSound.stop()
+        }
+        btnSound.play()
     }
+  
 
 
 }
